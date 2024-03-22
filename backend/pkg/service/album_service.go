fa9e4dc45376b2179c15665b94a1142c6364bebd
crysingzz <shuloly@mail.ru>
2024-03-22 18:48:26 +0300

    backend

--- backend/pkg/service/album_service.go
+++ backend/pkg/service/album_service.go
@@ -5,17 +5,13 @@ import (
 	"github.com/takwot/tech-strelka.git/pkg/models"
 )
 
-
 type AlbumService struct {
 	repo database.Repository
 }
 
-
 func NewAlbumService(repo database.Repository) *AlbumService {
 	return &AlbumService{repo: repo}
 }
-
-
 
 func (s *AlbumService) CreateAlbum(album models.Album) (int, error) {
 	return s.repo.Album.CreateAlbum(album)
