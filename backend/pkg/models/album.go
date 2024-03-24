5a461e1af47852909e1026522f4a1c6c88f9a17b
Kpatoc452 <108279554+Kpatoc452@users.noreply.github.com>
2024-03-25 02:06:34 +0300

    Update album.go

--- backend/pkg/models/album.go
+++ backend/pkg/models/album.go
@@ -3,7 +3,7 @@ package models
 
 type Album struct {
 	Id 			int 		`json:"id"`
-	Name 		string 		`json:"name`
+	Name 		string 		`json:"name"`
 	Author 		int 		`json:"author"`
 	Photos 		[]int 		`json:"photos"`
 }
